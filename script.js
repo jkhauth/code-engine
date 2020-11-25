0acbafa258fabfdb9ca6fa1510ea72b6ce66980e
jkhauth <jkhauth@gmail.com>
2020-11-25 16:22:12 -0500

    working choices for prompts added to button

--- script.js
+++ script.js
@@ -13,15 +13,22 @@ function writePassword() {
 }
 // Generating Password Functionality
 function generatePassword() {
-
+  var allUppercase = ["A", "B", "C", "D"]
+  var allChoices = []
   var passwordLength = prompt("How long would you like your password?");
-  
+    if (passwordLength >= 8 && 128){
+      //UPPERCASE PROMPT
+      var useUpper = confirm("Would you like uppercase?")
+        //IF USER CHOOSES UPPERCASE
+        if (useUpper === true){
+          allChoices.concat(allUppercase);
+      } else {
+        alert("YOU DONT WANT CAPS")
+      }
+      
 
-
-
-
-
-
+    }
+console.log(allChoices)
 }
 
 
