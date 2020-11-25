3f6bf8a06fcc7ad0f3c707f208b5da11e8cdbc8c
jkhauth <jkhauth@gmail.com>
2020-11-25 16:07:34 -0500

    add functionality to button and prompts how many characters

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
 
   passwordText.value = password;
 
-
-
 }
-
+// Generating Password Functionality
 function generatePassword() {
 
-  var lengthPass = prompt("How long do you want your password?");
-  var smallLetter = confirm("Does your password need lowercase characters?");
-  var bigLetter = confirm("Does your password need uppercase characters?");
-  var num = confirm("Does your password need numeric characters?");
-  var spec = confirm("Does your password need special characters in it?");
+  var passwordLength = prompt("How long would you like your password?");
+  
+
+
+
+
+
 
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", generatePassword);
