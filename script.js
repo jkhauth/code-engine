a027cae3cecaab21255a73793cadc8e560f9e1f7
John Hauth <jkhauth@gmail.com>
2020-11-25 21:58:21 -0500

    able to read a random criteria character

--- script.js
+++ script.js
@@ -15,37 +15,38 @@ function writePassword() {
 function generatePassword() {
   
     
-  var allChoices = ["hello"]
-  var allUppercase = ["A","B","D"];
   var passwordLength = prompt("How long would you like your password?");
-    if (passwordLength >= 8 && 128){
-            
-      //UPPERCASE PROMPT
-      var useUpper = confirm("Would you like uppercase?")
-        //IF USER CHOOSES UPPERCASE
-          if (useUpper === true) {
-          var upcase = allChoices.concat(allUppercase);
-      }
+
+  if (passwordLength >= 8 && 128){
+    //CRITERIA OF CHOICES
+    var allUppercase = ["A","B","D"];
+    var randomIndex = Math.floor(Math.random() * allUppercase.length);
+    var AllChoices = ["this"];
+
+    //UPPERCASE PROMPT
+    var useUpper = confirm("Would you like uppercase?")
+        if (useUpper === true) {
+                 
+        passwordText = allUppercase[randomIndex].concat(AllChoices);
+        alert(passwordText);        
+        }        
       //IF USER DOESN'T WANT UPPERCASE 
-      else {
+        else {
         alert("YOU DONT WANT CAPS");
-      }
-      
+        }
+        
     }
-
-
-
-
-
-
-
-    
+   
     //IF USER DOESN'T ENTER A VALID LENGTH
     else {
       alert("Please enter a valid length")
     }
 
-}
+} 
+
+
+
+//SENDS PASSWORD TO TEXT BOX
 
 
 // Add event listener to generate button
