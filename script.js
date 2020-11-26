2fe3059c66e536879ebf58ec8ac3fe762b100978
John Hauth <jkhauth@gmail.com>
2020-11-26 11:34:50 -0500

    put all the special characters in an array

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function generatePassword() {
           var allUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];  
           var allLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
           var allNums = ["0", "1", "2", "3", "4","5", "6", "7", "8", "9"]
-          var allSpec = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+          var allSpec = ['!', "\"", '#', "'", '$','%','&','(', ')', '*','+','-','.','//',':',';','<','=','>','?','@','[',']','^','_','`','{','|',"'", '}', '~',];
             // ALL RESPECTED OUTCOMES FROM DIFFERENT CHOICES
               var upperLower = allUppercase + allLowercase; 
               var upperNumber = allUppercase + allNums;
